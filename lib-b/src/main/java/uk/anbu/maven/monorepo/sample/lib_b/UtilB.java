6e50d27bdfc5a7a0000ff5582cf10d52eff20c66
anbu <git@anbu.work>
2024-08-19 05:34:23 +0100

    set versions

---
+++ lib-b/src/main/java/uk/anbu/maven/monorepo/sample/lib_b/UtilB.java
@@ -0,0 +1,9 @@
+package uk.anbu.maven.monorepo.sample.lib_b;
+
+public class UtilB {
+    public String getMessage() {
+        return "Hello from lib-b";
+    }
+
+    // something changed in lib-b, changed again 
+}
